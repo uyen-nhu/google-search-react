6d3289ac43c45983cb432f12665535c06b02c027
Uyen-Nhu Tran <125201939+uyen-nhu@users.noreply.github.com>
2023-04-17 10:22:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,62 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  let data = [
+    {
+      title: 'JavaScript Tutorial - W3School',
+      description: `Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java!`,
+      url: 'https://www.w3schools.com/js/',
+      links: [
+        {
+          title: 'JavaScript Introduction',
+          url: 'https://www.w3schools.com/js/js_intro.asp',
+        },
+        {
+          title: 'JS Functions',
+          url: 'https://www.w3schools.com/js/js_functions.asp',
+        },
+        {
+          title: 'JavaScript Examples',
+          url: 'https://www.w3schools.com/js/js_examples.asp',
+        },
+      ],
+    },
+    {
+      title: 'JavaScript | MDN',
+      description: `JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-className functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat.`,
+      url: 'developer.mozilla.org',
+      links: [
+        {
+          title: 'JavaScript Operators',
+          url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators',
+        },
+        {
+          title: 'JS Code',
+          url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference',
+        },
+        {
+          title: 'JavaScript Meaning',
+          url: 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics',
+        },
+      ],
+    },
+    {
+      title: 'JavaScript - Wikipedia',
+      description: `JavaScript often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time.`,
+      url: 'https://de.wikipedia.org/wiki/JavaScript',
+      links: [
+        {
+          title: 'JavaScript Online',
+          url: 'https://en.wikipedia.org/wiki/JavaScript#History',
+        },
+        {
+          title: 'JS Definition',
+          url: 'https://en.wikipedia.org/wiki/JavaScript#:~:text=JavaScript%20is%20a%20high%2Dlevel,functional%2C%20and%20imperative%20programming%20styles.',
+        },
+      ],
+    },
+  ]
+
   return (
     <div className="App">
       <nav>
